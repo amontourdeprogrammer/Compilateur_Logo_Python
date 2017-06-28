34b451a80a27253e752e41062b1842cd47c89f62
Korayaz <vanessaroux9@gmail.com>
2017-06-28 17:41:19 +0200

    add some tests in test_compilateur_logo && add function in compilateur_logo

--- compilateur_logo.py
+++ compilateur_logo.py
@@ -1,47 +1,66 @@
 def compile_logo(commands):
     html_commands = []
-    for command in commands :
-    	if command[0] == "repete":
-    		times = command[1];
-    		reapeated_commands = (deal_with_functions(deal_with_brackets(command)))
-    		result = "for (int i = 0; i < "+times+"; i = i+1){\n"+ reapeated_commands +"\n}"
-    		html_commands.append(result)
-    	elif command[0] == "origin":
-    		html_commands.append("origin();")
-    	else :
-       		html_commands.append ("{}({});".format(command[0], command[1]))
+    for command in commands:
+        html_commands.append(switch_board(command))
     return '\n'.join(html_commands)
 
 #given a command, extract the commands inside the brackets
 def deal_with_brackets(command):
-	for i in range(len(command)):
-		if "[" in command[i] :
-			start_index = i
-			bracket = command.pop(i)
-			no_bracket = ''.join(list(bracket)[1:])
-			command.insert(i,no_bracket)
-		elif "]" in command[i] :
-			end_index = i +1
-			bracket = command.pop(i)
-			no_bracket = ''.join(list(bracket)[:-1])
-			command.insert(i,no_bracket)
-	bracketless_command = command[start_index:end_index]
-	return bracketless_command
+    found_opening_bracket = False
+    found_closing_bracket = False
+    after_commands = []
+    while found_opening_bracket == False :
+        item_reviewed = command.pop(0)
+        if "[" in item_reviewed :
+            no_bracket = ''.join(list(item_reviewed)[1:])
+            command.insert(0,no_bracket)
+            found_opening_bracket = True
+    while found_closing_bracket == False :
+        item_reviewed = command.pop(-1)
+        if "]" in item_reviewed :
+            no_bracket = ''.join(list(item_reviewed)[:-1])
+            command.append(no_bracket)
+            found_closing_bracket = True
+        else :
+            after_commands.insert(0,item_reviewed)
+    result = [command,after_commands]
+    print 'deqling zith braket result'
+    print result
+    return result
 
 #given a command, extract the commands
 def deal_with_functions(command):
-	html_commands = []
-	known_functions_with_argument = ["av", "td", "tg"]
-	known_functions_without_argument = ['trait']
-	while len(command) >= 1 :
-		function_in_list = command.pop(0)
-		if function_in_list in known_functions_with_argument :
-			html_commands.append ("{}({});".format(function_in_list, command.pop(0)))
-		elif function_in_list in known_functions_without_argument : 
-			html_commands.append ("{}();".format(function_in_list))
-		else :
-			raise NameError("{} is not defined".format(command[i]))
-	return '\n'.join(html_commands)
+    html_commands = []
+    known_functions_with_argument = ["av", "td", "tg"]
+    known_functions_without_argument = ['origin']
+    while len(command) >= 1 :
+        function_in_list = command.pop(0)
+        if function_in_list in known_functions_with_argument :
+            html_commands.append ("{}({});".format(function_in_list, command.pop(0)))
+        elif function_in_list in known_functions_without_argument :
+            html_commands.append ("{}();".format(function_in_list))
+        else :
+            raise NameError("{} is not defined".format(function_in_list))
+    return '\n'.join(html_commands)
 
+#print compile_logo([["av","50"]])
 
+def switch_board(command):
+  
+    if command[0] == "repete":
+        times = command[1];
+        reapeated_commands = switch_board(deal_with_brackets(command)[0])
+        after_commands =''
+        if len(after_commands) == 0 :
+            after_commands =''
+        else :
+            after_commands = switch_board(deal_with_brackets(command)[1])
+         
+        print("for (int i = 0; i < "+times+"; i = i+1){\n"+ reapeated_commands +"\n}"+after_commands)
+        return "for (int i = 0; i < "+times+"; i = i+1){\n"+ reapeated_commands +"\n}"+after_commands
 
+    else :
+        return deal_with_functions(command)
+#repete 12 [repete 4 [av 20] td 30]
+print compile_logo([["repete", "12", "[repete", "4", "[av", "20]", "td", "30]"]])
+print deal_with_brackets(["repete", "12", "[repete", "4", "[av", "20]", "td", "30]"])
--- test_compilateur_logo.py
+++ test_compilateur_logo.py
@@ -2,17 +2,20 @@ import pytest
 from compilateur_logo import compile_logo, deal_with_brackets, deal_with_functions
 
 def test_compile_one_logo_command():
-    assert compile_logo([["ar","50"]]) == "ar(50);"
+    assert compile_logo([["av","50"]]) == "av(50);"
 def test_compile_several_logo_commands():
-    assert compile_logo([["ar","50"], ["td","90"]]) == "ar(50);\ntd(90);"
+    assert compile_logo([["av","50"], ["td","90"]]) == "av(50);\ntd(90);"
 def test_isolate_commands_in_brackets():
-	assert deal_with_brackets(["repete", "12", "[trait", "td", "30", "td", "50]"]) == ['trait', 'td', '30', 'td', '50']
+	assert deal_with_brackets(["repete", "12", "[origin", "td", "30", "td", "50]"]) == ['origin', 'td', '30', 'td', '50']
 def test_transform_logo_commands_in_processing():
-	assert deal_with_functions(['trait', 'td', '30', 'td', '50']) == "trait();\ntd(30);\ntd(50);"
+	assert deal_with_functions(['origin', 'td', '30', 'td', '50']) == "origin();\ntd(30);\ntd(50);"
 def test_compile_repete():
-    assert compile_logo([["repete", "12", "[trait", "td", "30]"]]) == "for (int i = 0; i < 12; i = i+1){\ntrait();\ntd(30);\n}"
+    assert compile_logo([["repete", "12", "[origin", "td", "30]"]]) == "for (int i = 0; i < 12; i = i+1){\norigin();\ntd(30);\n}"
 def test_compile_repete_with_other_commands():
-	assert compile_logo([["ar","50"], ["td","90"],["repete", "12", "[trait", "td", "30]"], ["ar","50"], ["td","90"]])== "ar(50);\ntd(90);\nfor (int i = 0; i < 12; i = i+1){\ntrait();\ntd(30);\n}\nar(50);\ntd(90);"
+	assert compile_logo([["av","50"], ["td","90"],["repete", "12", "[origin", "td", "30]"], ["av","50"], ["td","90"]])== "av(50);\ntd(90);\nfor (int i = 0; i < 12; i = i+1){\norigin();\ntd(30);\n}\nav(50);\ntd(90);"
+def test_compile_repete():
+    assert compile_logo([["repete", "12", "[repete", "4", "[av", "20]", "td", "30]"]]) == "for (int i = 0; i < 12; i = i+1){\nfor (int i = 0; i < 4; i = i+1){\nav(20);\n}\ntd(30);\n}"
+
 
 #def test_compile_if():
     #assert compile_logo([["si", "1", "<", "2" "[td", "30]"]]) == "if(1<2){\ntd(30);\n}"
